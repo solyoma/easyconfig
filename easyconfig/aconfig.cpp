c5da8f76637157d051e0908829854e3520bd50f4
András Sólyom <solyom@andreasfalco.com>
2020-01-25 12:57:39 +0100

    Moved function bodies into the cpp file, changed order of arguments, added constructor to base field class

--- easyconfig/aconfig.cpp
+++ easyconfig/aconfig.cpp
@@ -62,26 +62,130 @@ TEXT_FIELD& TEXT_FIELD::operator=(std::string newVal)
 	return *this;
 }
 
+void ACONFIG::AddBoolField(std::string name, bool defVal, bool val)
+{
+	BOOL_FIELD intf(name, defVal, val, this);
+	_boolList.push_back(intf);
+	if (_fields.count(name) && _fields[name]->kind != ackBool)
+		throw "type mismatch";
+	_fields[name] = &_boolList.back();
+}
+void ACONFIG::AddIntField(std::string name, int defVal, int val)
+{
+	INT_FIELD intf(name, defVal, val, this);
+	_intList.push_back(intf);
+	if (_fields.count(name) && _fields[name]->kind != ackInt)
+		throw "type mismatch";
+	_fields[name] = &_intList.back();
+}
+void ACONFIG::AddRealField(std::string name, double defVal, double val)
+{
+	REAL_FIELD intf(name, defVal, val, this);
+	_realList.push_back(intf);
+	if (_fields.count(name) && _fields[name]->kind != ackReal)
+		throw "type mismatch";
+	_fields[name] = &_realList.back();
+}
+void ACONFIG::AddTextField(std::string name, std::string defVal, std::string val)
+{
+	TEXT_FIELD textf(name, defVal, val, this);
+	size_t n = _textList.size();
+	_textList.push_back(textf);
+	if (_fields.count(name) && _fields[name]->kind != ackText)
+		throw "type mismatch";
+	_fields[name] = &_textList.back();
+}
+
+void ACONFIG::Load(std::string fname)	// from file
+{
+	_ifs.open(fname);
+	if (!_ifs.is_open())
+		throw "can't open";
+	std::vector < std::string> sl;
+	splitstring s;
+	while (!_ifs.eof())
+	{
+		_ifs >> s;
+		sl = s.split(',', 1);
+		if (sl.size() != 4) // <name>,<type>,<value>,<default>
+			throw "bad string";
+		switch (sl[1][0])
+		{
+		case 'b': AddBoolField(sl[0], stoi(sl[2]), stoi(sl[3])); break;
+		case 'i': AddIntField(sl[0], stoi(sl[2]), stoi(sl[3])); break;
+		case 'r': AddRealField(sl[0], stod(sl[2]), stod(sl[3])); break;
+		case 't': AddTextField(sl[0], sl[2], sl[3]);
+		default: break;
+		}
+	}
+	_ifs.close();
+}
+
+void ACONFIG::Store(std::string fname)
+{
+	_ofs.open(fname.c_str());
+	if (!_ofs.is_open())
+		throw "can't open";
+
+	for (auto p : _fields)
+		p.second->Store();
+	_ofs.close();
+}
+// DEBUG
+FIELD_BASE *ACONFIG::operator[](std::string fieldn)
+{
+
+	if (_fields.count(fieldn))
+		return _fields[fieldn];
+	return nullptr;
+}
+ACONFIG &ACONFIG::CopyFrom(const ACONFIG &other)
+{
+	if (_ifs.is_open())
+		_ifs.close();
+	if (_ofs.is_open())
+		_ofs.close();
+	_changed = other._changed;
+	_intList = other._intList;
+	_boolList = other._boolList;
+	_realList = other._realList;
+	_textList = other._textList;
+
+	_fields.clear();
+	for (auto a : _intList)
+		_fields[a.name] = &a;
+	for (auto a : _textList)
+		_fields[a.name] = &a;
+
+	return *this;
+}
+
 //--------------------------------------------------------------
+// writes field into open file in format
+// <name>,<kind (one LC letter)>,<default>,<actual value>
 
-void ACONFIG::WriteBool(BOOL_FIELD *pintf)
+void ACONFIG::WriteBool(BOOL_FIELD *pf)
 {
-	_ofs << pintf->name << ",b," << pintf->value << ","<<pintf->defVal << "\n";
+	_ofs << pf->name << ",b," << pf->defVal << ","<< pf->value << "\n";
+	pf->changed = false;
 }
 
-void ACONFIG::WriteInt(INT_FIELD *pintf)
+void ACONFIG::WriteInt(INT_FIELD *pf)
 {
-	_ofs << pintf->name << ",i," << pintf->value << "," << pintf->defVal << "\n";
+	_ofs << pf->name << ",i," << pf->defVal << "," << pf->value << "\n";
+	pf->changed = false;
 }
 
-void ACONFIG::WriteReal(REAL_FIELD *pintf)
+void ACONFIG::WriteReal(REAL_FIELD *pf)
 {
-	_ofs << pintf->name << ",r," << pintf->value << "," << pintf->defVal << "\n";
+	_ofs << pf->name << ",r," << pf->defVal << "," << pf->value << "\n";
+	pf->changed = false;
 }
 
-void ACONFIG::WriteText(TEXT_FIELD *ptextf)
+void ACONFIG::WriteText(TEXT_FIELD *pf)
 {
-	_ofs << ptextf->name << ",t," << ptextf->value << "," << ptextf->defVal << "\n";
+	_ofs << pf->name << ",t," << pf->defVal << "," << pf->value << "\n";
+	pf->changed = false;
 }
 
 void ACONFIG::DumpFields(ACONFIG_KIND kind, std::string file)
--- easyconfig/aconfig.h
+++ easyconfig/aconfig.h
@@ -54,10 +54,12 @@ std::string AconfigKindToString(ACONFIG_KIND);
 struct FIELD_BASE
 {
 	std::string name;
-	ACONFIG_KIND kind = ackNone;
+	ACONFIG_KIND kind;
 	ACONFIG *parent;
-	bool changed;
+	bool changed = false;
 	virtual void Store() = 0;	 // - " -
+
+	FIELD_BASE(std::string name, ACONFIG_KIND kind = ackNone, ACONFIG*parent = nullptr) : name(name), kind(kind), parent(parent) {}
 };
 
 struct BOOL_FIELD : public FIELD_BASE
@@ -65,7 +67,7 @@ struct BOOL_FIELD : public FIELD_BASE
 	bool value;
 	bool defVal;
 	BOOL_FIELD(std::string aname, bool aDefVal =false, bool val=false, ACONFIG* aparent = nullptr) :
-		value(val), defVal(aDefVal) {	name = aname; parent = aparent; kind = ackBool;	}
+		FIELD_BASE(aname, ackBool, aparent), value(val), defVal(aDefVal) {}
 	void Store();
 	std::string ToString() const { return value ? "TRUE" : "FALSE"; }
 	std::string DefToString() const { return defVal ? "TRUE" : "FALSE"; }
@@ -77,7 +79,7 @@ struct INT_FIELD : public FIELD_BASE
 	int value = 0;
 	int defVal = 0;		// if Load() is unsuccesfull use this
 	INT_FIELD(std::string aname, int defVal = 0, int val = 0, ACONFIG* aparent = nullptr) : 
-		value(val), defVal(defVal) { name = aname; parent = aparent; kind = ackInt;	}
+		FIELD_BASE(aname, ackInt, aparent), value(val), defVal(defVal) {}
 	void Store();
 	std::string ToString() const { return std::to_string(value); }
 	std::string DefToString() const { return std::to_string(defVal); }
@@ -89,9 +91,7 @@ struct REAL_FIELD : public FIELD_BASE
 	double value = 0;
 	double defVal = 0;		// if Load() is unsuccesfull use this
 	REAL_FIELD(std::string aname, double defVal = 0.0, double val = 0.0, ACONFIG* aparent = nullptr) :
-		value(val), defVal(defVal) {
-		name = aname; parent = aparent; kind = ackReal;
-	}
+		FIELD_BASE(aname, ackReal, aparent), value(val), defVal(defVal) {}
 	void Store();
 	std::string ToString() const { return std::to_string(value); }
 	std::string DefToString() const { return std::to_string(defVal); }
@@ -103,7 +103,7 @@ struct TEXT_FIELD : public FIELD_BASE
 	std::string value;
 	std::string defVal;		// if Load() is unsuccesfull use this
 	TEXT_FIELD(std::string aname, std::string aDefVal = std::string(), std::string val = std::string() , ACONFIG* aparent = nullptr) :
-		value(val), defVal(aDefVal) { name = aname; parent = aparent; kind = ackText;	}
+		FIELD_BASE(aname, ackInt, aparent), value(val), defVal(defVal) {}
 	void Store();
 	std::string ToString() const { return value; }
 	std::string DefToString() const { return defVal; }
@@ -126,109 +126,21 @@ public:
 
 	void SetChanged(bool changed) { _changed = changed; }
 
-	void AddBoolField(std::string name, bool defVal=false, bool val = false)
-	{
-		BOOL_FIELD intf(name, defVal, val, this);
-		_boolList.push_back(intf);
-		if (_fields.count(name) && _fields[name]->kind != ackBool)
-			throw "type mismatch";
-		_fields[name] = &_boolList.back();
-	}
-	void AddIntField(std::string name, int defVal=0, int val=0)
-	{
-		INT_FIELD intf(name, defVal, val, this);
-		_intList.push_back(intf);
-		if (_fields.count(name) && _fields[name]->kind != ackInt)
-			throw "type mismatch";
-		_fields[name] = &_intList.back();
-	}
-	void AddRealField(std::string name, double defVal = 0.0, double val = 0.0)
-	{
-		REAL_FIELD intf(name, defVal, val, this);
-		_realList.push_back(intf);
-		if (_fields.count(name) && _fields[name]->kind != ackReal)
-			throw "type mismatch";
-		_fields[name] = &_realList.back();
-	}
-	void AddTextField(std::string name, std::string defVal=std::string(), std::string val = std::string())
-	{
-		TEXT_FIELD textf(name, defVal, val, this);
-		size_t n = _textList.size();
-		_textList.push_back(textf);
-		if (_fields.count(name) && _fields[name]->kind != ackText)
-			throw "type mismatch";
-		_fields[name] = &_textList.back();
-	}
+	void AddBoolField(std::string name, bool defVal = false, bool val = false);
+	void AddIntField(std::string name, int defVal = 0, int val = 0);
+	void AddRealField(std::string name, double defVal = 0.0, double val = 0.0);
+	void AddTextField(std::string name, std::string defVal = std::string(), std::string val = std::string());
 
 	void WriteBool(BOOL_FIELD *pintf);
 	void WriteInt(INT_FIELD *pintf);
 	void WriteReal(REAL_FIELD *pintf);
 	void WriteText(TEXT_FIELD *ptextf);
 
-	void Load(std::string fname)	// from file
-	{
-		_ifs.open(fname);
-		if (!_ifs.is_open())
-			throw "can't open";
-		std::vector < std::string> sl;
-		splitstring s;
-		while (! _ifs.eof())
-		{
-			_ifs >> s;
-			sl = s.split(',',1);
-			if (sl.size() != 4) // <name>,<type>,<value>,<default>
-				throw "bad string";
-			switch (sl[1][0])
-			{
-				case 'b': AddBoolField(sl[0], stoi(sl[2]), stoi(sl[3])); break;
-				case 'i': AddIntField(sl[0], stoi(sl[2]), stoi(sl[3])); break;
-				case 'r': AddRealField(sl[0], stod(sl[2]), stod(sl[3])); break;
-				case 't': AddTextField(sl[0], sl[2], sl[3]);
-				default: break;
-			}
-		}
-		_ifs.close();
-	}
-
-	void Store(std::string fname)
-	{
-		_ofs.open(fname.c_str());
-		if (!_ofs.is_open())
-			throw "can't open";
-
-		for (auto p : _fields)
-			p.second->Store();
-		_ofs.close();
-	}
+	void Load(std::string fname);	// from file
+	void Store(std::string fname);
 		// DEBUG
 	void DumpFields(ACONFIG_KIND kind = ackNone, std::string file=std::string());	// print all
 		// operators
-	FIELD_BASE *operator[](std::string fieldn)
-	{
-
-		if (_fields.count(fieldn))
-			return _fields[fieldn];
-		return nullptr;
-	}
-	ACONFIG &CopyFrom(const ACONFIG &other)
-	{
-		if (_ifs.is_open())
-			_ifs.close();
-		if (_ofs.is_open())
-			_ofs.close();
-		_changed = other._changed;
-		_intList = other._intList;
-		_boolList = other._boolList;
-		_realList = other._realList;
-		_textList = other._textList;
-
-		_fields.clear();
-		for (auto a : _intList)
-			_fields[a.name] = &a;
-		for (auto a : _textList)
-			_fields[a.name] = &a;
-
-		return *this;
-	}
-
+	FIELD_BASE *operator[](std::string fieldn);
+	ACONFIG &CopyFrom(const ACONFIG &other);
 };
